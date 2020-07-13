d0b8c769f78649b97abc444650a7e9cfc270bcd2
Seby_Plays <46566740+SebyPlays@users.noreply.github.com>
2020-07-14 01:14:29 +0200

    Add files via upload

--- consoleprinter-api/src/de/github/sebyplays/consoleprinterapi/api/ConsolePrinter.java
+++ consoleprinter-api/src/de/github/sebyplays/consoleprinterapi/api/ConsolePrinter.java
@@ -5,6 +5,8 @@ import de.github.sebyplays.consoleprinterapi.utils.SystemProperty;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
+import java.io.Console;
+
 import static org.fusesource.jansi.Ansi.Color.*;
 import static org.fusesource.jansi.Ansi.ansi;
 
@@ -27,6 +29,22 @@ public class ConsolePrinter {
         return;
     }
 
+    public static String getString(String string, boolean colored, boolean time){
+        DateUtil dateUtil = new DateUtil();
+        if (colored){
+            if(consolePrinter.systemProperty.getOS() == OperatingSystem.WINDOWS || consolePrinter.systemProperty.getOS() == OperatingSystem.LINUX){
+                consolePrinter.install();
+                string = consolePrinter.colorize(string);
+                consolePrinter.uninstall();
+            } else {
+                ConsolePrinter.print("Your System isn't officially supported yet!\n           Some features might not be available due to an unidentified OS.\n           Please report this incident here:\n           https://github.com/sebyplays/Java-ConsolePrinter-API/issues", false, true);
+            }
+        }
+        String processedString;
+        if(time){ processedString = dateUtil.getTime() + string; }else{ processedString = string;}
+        return processedString;
+    }
+
     private void install(){
         AnsiConsole.systemInstall();
         return;
@@ -43,39 +61,39 @@ public class ConsolePrinter {
         for(int i = 0; i < splitMessage.length; i++){
             if(splitMessage[i].contains("§0")){
                 splitMessage[i] = splitMessage[i].replaceAll("§0","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(BLACK).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(BLACK).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§1")){
                 splitMessage[i] = splitMessage[i].replaceAll("§1","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(RED).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(RED).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§2")){
                 splitMessage[i] = splitMessage[i].replaceAll("§2","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(GREEN).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(GREEN).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§3")){
                 splitMessage[i] = splitMessage[i].replaceAll("§3","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(YELLOW).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(YELLOW).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§4")){
                 splitMessage[i] = splitMessage[i].replaceAll("§4","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(BLUE).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(BLUE).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§5")){
                 splitMessage[i] = splitMessage[i].replaceAll("§5","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(MAGENTA).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(MAGENTA).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§6")){
                 splitMessage[i] = splitMessage[i].replaceAll("§6","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(CYAN).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(CYAN).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§7")){
                 splitMessage[i] = splitMessage[i].replaceAll("§7","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(WHITE).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(WHITE).a(splitMessage[i]).fg(DEFAULT));
             }
             if(splitMessage[i].contains("§8")){
                 splitMessage[i] = splitMessage[i].replaceAll("§8","");
-                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(DEFAULT).a(splitMessage[i]));
+                splitMessage[i] = String.valueOf(ansi().eraseScreen().fg(DEFAULT).a(splitMessage[i]).fg(DEFAULT));
             }
             result = result + splitMessage[i] + " ";
         }
